f1633dec395c13e2888942d64296030787db31a6
nyigoro <nyigoro@gmail.com>
2025-07-08 10:29:14 +0300

    Create main.ts

---
+++ gui/electron/main.ts
@@ -0,0 +1,17 @@
+// gui/electron/main.ts
+import { app, BrowserWindow } from 'electron';
+import path from 'path';
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadURL('http://localhost:5173');
+}
+
+app.whenReady().then(createWindow);
